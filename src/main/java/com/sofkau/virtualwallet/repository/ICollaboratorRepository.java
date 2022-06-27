47da36dee1f9ddd116e7f81456264cd282ffcd08
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-06-27 15:33:16 -0500

    pull de la rama 'sher', conflictos resueltos, nombres cambiados a ingles

--- src/main/java/com/sofkau/virtualwallet/repository/ICollaboratorRepository.java
+++ src/main/java/com/sofkau/virtualwallet/repository/ICollaboratorRepository.java
@@ -3,5 +3,5 @@ package com.sofkau.virtualwallet.repository;
 import com.sofkau.virtualwallet.collection.Collaborator;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface IColaboradorRepository extends ReactiveMongoRepository<Collaborator, String> {
+public interface ICollaboratorRepository extends ReactiveMongoRepository<Collaborator, String> {
 }
