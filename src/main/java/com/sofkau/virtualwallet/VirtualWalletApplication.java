ec2e683d2a32933bae4b15639d423122b67a144f
Sherilyn Maestre <you@example.com>
2022-06-27 09:28:21 -0500

    initializing project

---
+++ src/main/java/com/sofkau/virtualwallet/VirtualWalletApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.virtualwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/virtualwallet/VirtualWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.virtualwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
